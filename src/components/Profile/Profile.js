23e543bc488584ae382a750303d836e6fc3402f3
Oleksiy Nikolaenko <alosha.nikolanko333@gmail.com>
2021-06-18 23:40:48 +0300

    add dialogs component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,8 +3,8 @@ import s from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className={s.content}>
-            <img className={s.content_img} src='https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300' alt='logo'/>
+        <div>
+            <img className={s.img} src='https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300' alt='logo'/>
             <div>ava + description</div>
             <MyPosts/>
         </div>
